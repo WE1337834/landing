3f969b962aded8ec1c853d3668c60a9e4419b7c5
WE1337834 <kuzya.mironov.04@gmail.com>
2024-01-12 22:51:26 +0500

    ...

--- src/components/Lesson.js
+++ src/components/Lesson.js
@@ -12,10 +12,10 @@ class Lesson extends React.Component{
                         занятие прямо сейчас!
                     </h2>
                     <form className="Lesson-form">
-                        <input type="text"   className="Lesson-fi"   placeholder="Имя ученика" name="Student_name" id="Lesson-Student_name" />
-                        <input type="text"   className="Lesson-fi"   placeholder="Имя родителя" name="Parent_name" id="Lesson-Parent_name" />
-                        <input type="tel"    className="Lesson-fi"   placeholder="+7 (999) 999-99-99" name="tel" id="Lesson-tel" />
-                        <input type="email"  className="Lesson-fi"   placeholder="Email (необязательно)" name="email" id="Lesson-email" />
+                        <input type="text" className="Lesson-fi"   placeholder="Имя ученика" name="Student_name" id="Lesson-Student_name" />
+                        <input type="text" className="Lesson-fi"   placeholder="Имя родителя" name="Parent_name" id="Lesson-Parent_name" />
+                        <input type="tel" className="Lesson-fi"   placeholder="+7 (999) 999-99-99" name="tel" id="Lesson-tel" />
+                        <input type="email" className="Lesson-fi"   placeholder="Email (необязательно)" name="email" id="Lesson-email" />
                         <input type="submit" className="Lesson-btn"  value="Записаться на пробный урок" />
                     </form>
                     <p className="Lesson-politics">
